92e11bb442229b52174e85b6b9db725f627da0b1
Sudarshanj10 <154816706+Sudarshanj10@users.noreply.github.com>
2024-10-01 19:35:36 +0530

    Add files via upload

---
+++ Myclass2.java
@@ -0,0 +1,10 @@
+class  Myclass2{
+	public static void main(String args[]){
+	//System.out.println("The first parameter is " +args[0]);
+	//System.out.println("The Second parameter is "+args[1]);
+	//System.out.println("The thiird parameter is " +args[2]);
+	//System.out.println("The total parameter are " +args.length);
+	System.out.println("total is:" +(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+	
+}
+}
